0de6b003f193296bd4e56050d7eb42ca56f830cf
Melita-Carty <Melita.Carty@digital.homeoffice.gov.uk>
2025-12-16 15:24:52 +0000

    Add user stories

---
+++ BankAccount/user_stories.md.m
@@ -0,0 +1,13 @@
+
+
+
+User Story 1:
+
+As a user of the app, I want to be able to see a log of transactions (including deposits and withdrawals).
+- AC001: Each transaction includes the following information:
+    - The value
+    - The datetime when the transaction occurred
+    - The value of the account before the transaction
+    - The value of the account after the transaction
+
+- AC002: I can go into the app and view a bank statement which shows me all the transactions between two given dates, and the start and end values of the account for this period
